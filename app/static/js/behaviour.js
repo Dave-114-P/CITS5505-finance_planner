2f2e5b0ddc5084674ce7154f571cb6860cccbf1d
Karl_Sue <tuankhanh17032003@gmail.com>
2025-04-28 20:04:50 +0800

    display the information of university of related states

--- app/static/js/behaviour.js
+++ app/static/js/behaviour.js
@@ -22,11 +22,17 @@ document.addEventListener("DOMContentLoaded", () => {
         const bbox = state.getBBox(); // Get the bounding box of the state
 
         // Get the center of the bounding box to center the zoom on the clicked state
-        const stateCenterX = bbox.x + bbox.width / 2;
-        const stateCenterY = bbox.y + bbox.height / 2;
+        let stateCenterX = bbox.x + bbox.width / 2;
+        let stateCenterY = bbox.y + bbox.height / 2;
+
+        // Adjust the center for Tasmania (AU-TAS) to ensure it is centered correctly
+        if (state.id === "AU-TAS") {
+            stateCenterX -= 40; // move 30 units to the right
+            stateCenterY -= 55; // move 50 units downward
+        }
 
         // Calculate the new viewBox values
-        const zoomFactor = 1.5; // Adjust this to control the zoom level
+        const zoomFactor = (state.id !== "AU-TAS") ? 1.5:2; // Adjust this to control the zoom level
         const newWidth = 450 / zoomFactor; // 800 is the original width
         const newHeight = 450 / zoomFactor; // 600 is the original height
 
@@ -68,6 +74,19 @@ document.addEventListener("DOMContentLoaded", () => {
         });
         state.style.fill = '#76c87d'; // fill the clicked state with a different color
         state.style.opacity = 1; // Keep the clicked state fully visible
+
+        document.querySelector('#state_uni_info').style.display = 'block';
+
+        const allStateInfos = document.querySelectorAll('#state_uni_info > div');
+        allStateInfos.forEach(info => {
+            info.style.display = 'none';
+        });
+
+        const stateName = state.id;
+        const stateInfo = document.querySelector(`.${stateName}`);
+        if (stateInfo) {
+            stateInfo.style.display = 'flex';
+        }
       });
     });
 
