16f2fa14446b0aa2165e43ee09b58d6594c9c3b5
Maxwell2048 <139220610+Maxwell2048@users.noreply.github.com>
2025-05-05 02:08:43 +0800

    Update categories.py

--- app/models/categories.py
+++ app/models/categories.py
@@ -5,8 +5,10 @@ class Category(db.Model):
     __tablename__ = "categories"
 
     # Columns
-    id = db.Column(db.String(30), primary_key=True)
-    category = db.Column(db.String(50), nullable=False, unique=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    category = db.Column(db.String(50), nullable=False)
+    budget = db.Column(db.Float, nullable=False)
+    lifestyle = db.Column(db.String(20), nullable=False)
 
     # Relationship with Spendings
     spendings = db.relationship("Spending", backref="category", lazy=True)
@@ -14,6 +16,8 @@ class Category(db.Model):
     def __repr__(self):
         return f"<Category ID {self.id}: {self.category}>"
     
-    def __init__(self, category, id):
+    def __init__(self, category, budget, lifestyle, id=None):
         self.id = id
         self.category = category
+        self.budget = budget
+        self.lifestyle = lifestyle
