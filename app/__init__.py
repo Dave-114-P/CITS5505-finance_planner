d483430939c3c7441d8c1d8bba9f035fb9790ce1
Karl_Sue <tuankhanh17032003@gmail.com>
2025-05-11 23:31:49 +0800

    resolve error of missing os

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
+import os
 
 # Initialize extensions
 db = SQLAlchemy()
