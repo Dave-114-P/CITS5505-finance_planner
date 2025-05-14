f3c812543f8218f4425eb385cc7fac5056fa03f1
Karl_Sue <tuankhanh17032003@gmail.com>
2025-05-14 22:13:41 +0800

    create all db model

--- app/__init__.py
+++ app/__init__.py
@@ -67,5 +67,9 @@ def create_app(config=None):
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
+    
+     # Import routes after initializing app and db
+    with app.app_context():
+        db.create_all()
 
     return app
